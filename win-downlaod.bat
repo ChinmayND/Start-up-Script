fb6687b0d6d4d08e6756bada66dd94654e406312
ChinmayND <191255095+ChinmayND@users.noreply.github.com>
2025-04-14 15:21:53 +0000

    Update win-downlaod.bat

--- win-downlaod.bat
+++ win-downlaod.bat
@@ -5,12 +5,12 @@ setlocal enabledelayedexpansion
 :: List your Winget app IDs here, one per line
 :: ==========================================
 
-set "app1=valinet.ExplorerPatcher"
-set "app2=microsoft.powertoys"
+set "app1=microsoft.powershell" 
+set "app2=microsoft.appinstaller"
 set "app3=AutoHotkey.AutoHotkey"
-set "app4=vscodium.vscodium"
+set "app4=valinet.ExplorerPatcher"
 set "app5=Valve.Steam"
-set "app6=VideoLAN.VLC"
+set "app6=vscodium.vscodium"
 set "app7=Neovim.Neovim"
 set "app8=RaspberryPiFoundation.RaspberryPiImager"
 set "app9=Oracle.VirtualBox"
@@ -18,12 +18,19 @@ set "app10=KDE.Krita"
 set "app11=BlenderFoundation.Blender"
 set "app12=KDE.KDEConnect"
 set "app13=logseq.logseq"
-set "app14=Nvidia.GeForceExperience"
-set "app15=Voicemod.Voicemod"
-set "app16=WhatsApp.WhatsApp"
+set "app14=KiCad.KiCad"
+set "app15=Microsoft.PowerToys"
+set "app16=9NKSQGP7F2NH" ::whatsapp
 set "app17=VB-Audio.Voicemeeter.Potato"
 set "app18=Audacity.Audacity"
-set "app19=fastfetch"
+set "app19=Inkscape.inksacpe"
+set "app20=OBSProject.OBSStudio"
+set "app21=XP9B0BH6T8Z7KZ"  ::voicemod
+set "APP22=VideoLAN.VLC"
+set "app23=glzr-io.GlazeWM"
+set "app24=Nvidia.GeForceExperience"
+set "app20=fastfetch-cli.fastfetch"
+:: SET "app21=winget install FilesCommunity.Files"
 
 :: Total number of apps to install
 set "total=19"
